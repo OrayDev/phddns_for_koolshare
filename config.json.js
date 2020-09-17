b932af5069d0d274b5a2ac2929bf26e259c38f85
sadoneli <sadoneli@gmail.com>
2020-09-17 10:56:54 +0800

    add ZenWiFi_AX6600 & ZenWiFi_XD4 support

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "花生壳内网版3.0 （phddns） 动态域名 内网穿透", 
     "home_url": "Module_phddns.asp", 
-    "md5": "e99967b7a26d08c2cc90c65a34ed3bf2", 
+    "md5": "c67c1f1f4781f0bc5d7355c220ccf153", 
     "module": "phddns", 
     "title": "花生壳3.0", 
     "version": "3.0.2"
