eaa361e64b68979d12fee4d12fe362e9135a840c
lxt <lvxiangting@oray.com>
2020-10-10 11:50:36 +0800

    修复自启动不好用的bug

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "花生壳内网版3.0 （phddns） 动态域名 内网穿透", 
     "home_url": "Module_phddns.asp", 
-    "md5": "8b35398747c95e3c569980c450de76df", 
+    "md5": "885de2496b161fa8162cb8afb332e476", 
     "module": "phddns", 
     "title": "花生壳3.0", 
     "version": "3.0.2"
--- phddns/init.d/S60Phddns.sh
+++ phddns/init.d/S60Phddns.sh
@@ -5,7 +5,7 @@ Phddns=/koolshare/phddns
 proc=`ps | grep oraysl | grep -v "grep" | grep -v "grep"|awk '{print $1}'`
 start_with_sys(){
 	if [ -z "$proc" ]; then
-		/koolshare/script/phddns_run.sh  start >/dev/null 2>&1 &
+		/koolshare/scripts/phddns_run.sh  start >/dev/null 2>&1 &
 	fi
 }
 case $1 in
