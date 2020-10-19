31958902cb4699ae8d26b2abeb9e5ee1bf0cf7d2
sadoneli <sadoneli@gmail.com>
2020-10-19 15:17:24 +0800

    upate for new router

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "花生壳内网版3.0 （phddns） 动态域名 内网穿透", 
     "home_url": "Module_phddns.asp", 
-    "md5": "885de2496b161fa8162cb8afb332e476", 
+    "md5": "15e7dc802b36873f470e59a73bd7e412", 
     "module": "phddns", 
     "title": "花生壳3.0", 
-    "version": "3.0.2"
+    "version": "3.0.3"
 }
